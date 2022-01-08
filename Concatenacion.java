d92cc5e2708279cd3307492352b3396b8ed4fd69
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-01-07 21:25:11 -0500

    Add file Concatenacion.java

---
+++ Concatenacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concatenacion;
+
+
+public class Concatenacion {
+
+    
+    
+    public static void main(String[] args) {
+      
+   String A;A="Juliana";
+   String B;B="Castillo";
+   String C;C="Araujo";
+   String D;D="Code:261120112";
+   
+        System.out.println("My name is "+A+ " " +B+ " " +C+ " "  +D);
+    }
+    
+}
